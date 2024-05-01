558498d4ac4eff397d273dfd4e9c482ea5c28fa0
iniyavasanthan <iniyavasanthan123@gmail.com>
2024-05-01 22:10:39 +0530

    Fixed input handling in hlo.java

--- hlo.java
+++ hlo.java
@@ -4,19 +4,14 @@ public class hlo {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter a string: ");
-        String str = scanner.nextLine();
-
-        boolean isPalindrome = true;
-        for (int i = 0; i < str.length() / 2; i++) {
-            if (str.charAt(i) != str.charAt(str.length() - 1 - i)) {
-                isPalindrome = false;
-                break;
-            }
-        }
-        if (isPalindrome) {
-            System.out.println(str + " is a palindrome.");
+        
+        if (scanner.hasNextLine()) { // Check if input is available
+            String input = scanner.nextLine();
+            System.out.println("You entered: " + input);
         } else {
-            System.out.println(str + " is not a palindrome.");
+            System.out.println("No input provided.");
         }
+        
+        scanner.close(); // Close the scanner
     }
 }
